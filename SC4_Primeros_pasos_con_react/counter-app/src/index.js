4eabfded29b3b0e0cea83200a794085c8128ee58
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2021-10-14 13:08:54 +0200

    modificando app react

--- SC4_Primeros_pasos_con_react/counter-app/src/index.js
+++ SC4_Primeros_pasos_con_react/counter-app/src/index.js
@@ -1,1 +1,9 @@
-console.log('hi')
+import React from 'react'
+import  ReactDOM  from 'react-dom';
+
+const divRoot = document.querySelector("#root")
+
+const saludo = <h1>Hola Mundo</h1>
+
+ReactDOM.render(saludo,divRoot)
+
