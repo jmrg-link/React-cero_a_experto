6f7e6dadcb56cc7ac234567f119b9ee829814af3
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2021-10-15 08:26:22 +0200

    cambio en primeraapp.js

--- SC4_Primeros_pasos_con_react/counter-app/src/PrimeraApp.js
+++ SC4_Primeros_pasos_con_react/counter-app/src/PrimeraApp.js
@@ -8,7 +8,7 @@ const PrimeraApp = ( props ) => {
   return (
     <>
       <h1>{ saludo }</h1>
-      <pre>{/* JSON.stringify(saludo , null , 3) */}</pre>
+     
       <p>Mi Primera aplicacion</p>
     </>
   );
