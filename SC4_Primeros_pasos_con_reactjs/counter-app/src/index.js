5f4adf806d7f44f22c3251f30e619640801017df
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2022-05-11 23:40:56 +0200

    SC4:upgrade React 18

--- SC4_Primeros_pasos_con_reactjs/counter-app/src/index.js
+++ SC4_Primeros_pasos_con_reactjs/counter-app/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import PrimeraApp from './PrimeraApp';
 
 import './index.css'
 
 const divRoot = document.querySelector('#root')
-ReactDOM.render(<PrimeraApp saludo='Hola Mundo'/>,divRoot)
+const root = createRoot(divRoot)
+root.render(<PrimeraApp saludo='Hola Mundo'/>)
