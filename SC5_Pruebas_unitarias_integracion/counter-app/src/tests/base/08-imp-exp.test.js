d2401404c7d419060c17b852a85bc55d33499c82
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2022-05-23 22:59:29 +0200

    Test Sc5:jest

--- SC5_Pruebas_unitarias_integracion/counter-app/src/tests/base/08-imp-exp.test.js
+++ SC5_Pruebas_unitarias_integracion/counter-app/src/tests/base/08-imp-exp.test.js
@@ -1,8 +1,10 @@
-import {  } from "../../base/"
+import { getHeroeById } from "../../base/08-imp-exp"
 import "@testing-library/jest-dom";
 
-describe('', () => { 
-    test('', () => { 
-        
+describe('Pruebas en function de heroes', () => { 
+    test('debe de retornar un heroe por id', () => { 
+        const id = 1
+        const heroe = getHeroeById(id)
+        console.log(heroe);
     })
  })
