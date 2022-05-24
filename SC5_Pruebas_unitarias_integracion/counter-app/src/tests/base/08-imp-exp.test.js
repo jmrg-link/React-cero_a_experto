8cebac54354707a1a2feae8f8d8b5cd8908952bd
Jesus Mª Rico Gonzalez <52455810+jmrg-link@users.noreply.github.com>
2022-05-24 14:14:37 +0200

    SC5: test jest heroes

--- SC5_Pruebas_unitarias_integracion/counter-app/src/tests/base/08-imp-exp.test.js
+++ SC5_Pruebas_unitarias_integracion/counter-app/src/tests/base/08-imp-exp.test.js
@@ -1,10 +1,38 @@
-import { getHeroeById } from "../../base/08-imp-exp"
+
+import { getHeroeById, getHeroesByOwner } from "../../base/08-imp-exp"
+import heroes from '../../data/heroes'
+
 import "@testing-library/jest-dom";
 
 describe('Pruebas en function de heroes', () => { 
     test('debe de retornar un heroe por id', () => { 
-        const id = 1
+        const id = 2
         const heroe = getHeroeById(id)
-        console.log(heroe);
+        const heroeData = heroes.find(p => p.id === id)
+        expect(heroe).toEqual(heroeData)
+        console.log(heroeData);
+        //console.log(heroe);
     })
+
+    test('debe de retornar un undefined si Heroe no existe', () => { 
+        const id = 10 
+        const heroe = getHeroeById(10)
+        //console.log(heroe);
+        expect(heroe).toBe(undefined)
+     })
+
+    test('debe de retornar un arreglo con los heroes de Dc', () => { 
+        const owner  = 'DC'
+        const heroes = getHeroesByOwner(owner)
+        const heroesData = heroes.filter(p => p.owner === owner)
+        //console.log(heroes);
+        //console.log(heroesData)
+        expect(heroes).toEqual(heroesData)
+      })
+    
+    test('debe de retornar un arreglo con los heroes de Marvel', () => { 
+        const owner  = 'Marvel'
+        const heroes = getHeroesByOwner(owner)
+        expect(heroes.length).toBe(2)
+    })  
  })
