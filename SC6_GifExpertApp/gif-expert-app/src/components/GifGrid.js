9485617c4ebed97915aea16bbf21d83b4ee93ef1
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2022-06-01 00:00:12 +0200

    new sc6:add helpers

--- SC6_GifExpertApp/gif-expert-app/src/components/GifGrid.js
+++ SC6_GifExpertApp/gif-expert-app/src/components/GifGrid.js
@@ -1,31 +1,16 @@
 import React ,{ useState , useEffect } from 'react'
-import config from '../config/index.js'
 import { GifGridItem } from './GifGridItem.js'
+import { getGifts } from '../helpers/GetGifs.js'
 
 export const GifGrid = ({category}) => {
 
   const [ images , setImages ] = useState([])
-
   
   useEffect( ()=>{
-    getGifts()
-  }, [])
-
-    const getGifts = async() => {
-        const url = `https://api.giphy.com/v1/gifs/search?q=morty&limit=10&api_key=${config.apikey}`
-        const resp = await fetch(url)
-        const { data } = await resp.json()
-        const gifs = data.map(img => {
-          return {
-            id:img.id,
-            title:img.title,
-            url:img.images?.downsized_medium.url
-          }
-        })
-        console.log(gifs);
-        setImages(gifs)
-    }
-    //getGifts()
+    getGifts( category )
+      .then( setImages)
+  }, [category])
+    
   return (
     <>
       <h3>{ category }</h3>
