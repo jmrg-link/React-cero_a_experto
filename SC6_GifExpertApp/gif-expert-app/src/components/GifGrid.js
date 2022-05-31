ffbf48790b9da572da19fabebcd2849142810e91
Jesus Mª Rico Gonzalez <52455810+jmrg-link@users.noreply.github.com>
2022-05-31 14:03:34 +0200

    SC6: app gifs react18

--- SC6_GifExpertApp/gif-expert-app/src/components/GifGrid.js
+++ SC6_GifExpertApp/gif-expert-app/src/components/GifGrid.js
@@ -1,18 +1,42 @@
-import React from 'react'
-import config from '../../../../config/index.mjs'
+import React ,{ useState , useEffect } from 'react'
+import config from '../config/index.js'
+import { GifGridItem } from './GifGridItem.js'
 
 export const GifGrid = ({category}) => {
-    
+
+  const [ images , setImages ] = useState([])
+
+  
+  useEffect( ()=>{
+    getGifts()
+  }, [])
+
     const getGifts = async() => {
         const url = `https://api.giphy.com/v1/gifs/search?q=morty&limit=10&api_key=${config.apikey}`
-        const resp = fetch(url)
-        const data = await resp.json()
-        console.log(data);
-
+        const resp = await fetch(url)
+        const { data } = await resp.json()
+        const gifs = data.map(img => {
+          return {
+            id:img.id,
+            title:img.title,
+            url:img.images?.downsized_medium.url
+          }
+        })
+        console.log(gifs);
+        setImages(gifs)
     }
+    //getGifts()
   return (
     <>
         <h3>{ category }</h3>
+          {
+            images.map( img => (
+              <GifGridItem 
+                key={img.id}
+                { ...img }
+              />
+            ))
+          }
     </>
   )
 }
---
+++ SC6_GifExpertApp/gif-expert-app/src/config/index.js
@@ -0,0 +1,5 @@
+const config = {
+    apikey: process.env.REACT_APP_APIKEY
+}  
+
+export default config
