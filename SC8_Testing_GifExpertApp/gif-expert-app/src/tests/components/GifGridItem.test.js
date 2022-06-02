e2d2e3d351e9db677b79e40d3d8ff824db1c95e7
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2022-06-02 22:32:54 +0200

    add SC8:Testing GifExpertApp.test.js

--- SC8_Testing_GifExpertApp/gif-expert-app/src/tests/components/GifGridItem.test.js
+++ SC8_Testing_GifExpertApp/gif-expert-app/src/tests/components/GifGridItem.test.js
@@ -2,8 +2,10 @@ import { shallow } from "enzyme";
 import { GifGridItem } from "../../components/GifGridItem";
 
 describe("Pruebas en <GifGridItem />", () => {
+    const title = 'Un titulo'
+    const url   = 'https://localhost/ejemplo.jpeg'
   test("debe de mostrar el componente correctamente", () => {
-    const wrapper = shallow(<GifGridItem />);
+    const wrapper = shallow(<GifGridItem title={title} url={url}/>);
     expect(wrapper).toMatchSnapshot();
   });
 });
