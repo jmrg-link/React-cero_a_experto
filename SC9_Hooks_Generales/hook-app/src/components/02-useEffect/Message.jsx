d314c93e1c4980d831973c4629140f5625a8b2b8
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2022-06-13 01:01:09 +0200

    add SC9:Hooks - reset hook

--- SC9_Hooks_Generales/hook-app/src/components/02-useEffect/Message.jsx
+++ SC9_Hooks_Generales/hook-app/src/components/02-useEffect/Message.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export const Message = () => {
+  const [coords,setCoords] = useState({x:0,y:0})
+  useEffect(()=>{
+      const onMouseMove = ({ x , y }) => {
+        //const coords = { x , y }
+        setCoords({x,y})
+      }
+      window.addEventListener('mousemove', onMouseMove)
+
+    return () => {
+
+      window.removeEventListener('mousemove', onMouseMove)  
+    }
+  },[])
   return (
-    <div>Message</div>
-  )
-}
+    <>
+      <h3>Usuario ya existe...</h3>
+      {
+        JSON.stringify(coords)
+      }
+    </>
+  );
+};
