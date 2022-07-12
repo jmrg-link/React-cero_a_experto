66344dc6cda9315844d4c8d1d63c852bea94428e
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2022-07-12 09:50:06 +0200

    add SC10: Init useReducer : Example 2 : TodoApp delete todo

--- SC10_Hooks_useReducer/hook-app-useReducer/src/08-useReducer/TodoApp.jsx
+++ SC10_Hooks_useReducer/hook-app-useReducer/src/08-useReducer/TodoApp.jsx
@@ -1,23 +1,21 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import { TodoAdd } from "./TodoAdd";
 import { TodoList } from "./TodoList";
 import { todoReducer } from "./todoReducer";
 
-const initialState = [
-  {
-    id: new Date().getTime(),
-    description: "Recolectar datos primarios ID:1",
-    done: false,
-  },
-  {
-    id: new Date().getTime() + 100,
-    description: "Recolectar datos secundarios ID:2",
-    done: false,
-  },
-];
+const initialState = [];
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
 
 export const TodoApp = () => {
-  const [todos, dispatch] = useReducer(todoReducer, initialState);
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   const handleNewTodo = (todo) => {
     const action = {
       type: "[TODO] Add Todo",
