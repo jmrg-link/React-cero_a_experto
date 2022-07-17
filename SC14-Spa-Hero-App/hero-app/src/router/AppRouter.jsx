541745fdd6abd26474f2644fcd559361a924e5a8
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2022-07-17 17:53:12 +0200

    SC14:Create folder and structure hero app

---
+++ SC14-Spa-Hero-App/hero-app/src/router/AppRouter.jsx
@@ -0,0 +1,20 @@
+import { Route , Routes , Navigate } from "react-router-dom"
+import { Navbar } from '../ui'
+import { LoginPage } from "../auth"
+import { DcPage, MarvelPage } from "../heroes"
+
+
+export const AppRouter = () => {
+  return (
+    <>
+        <Navbar/>
+        <Routes>
+            <Route path="marvel" element={<MarvelPage/>}/>
+            <Route path="dc"     element={<DcPage/>}/>
+            <Route path="login"  element={<LoginPage/>}/>
+            
+            <Route path="/" element={<Navigate to="/marvel"/>}/>
+        </Routes>
+    </>
+  )
+}
