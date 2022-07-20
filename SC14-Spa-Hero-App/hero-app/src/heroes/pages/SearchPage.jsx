ea6033b986dbf787ecd344f9df33eaca768b82d2
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2022-07-20 02:25:49 +0200

    SC14:Create heroPage and result front read data and pictures

--- SC14-Spa-Hero-App/hero-app/src/heroes/pages/SearchPage.jsx
+++ SC14-Spa-Hero-App/hero-app/src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,26 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import queryString from "query-string";
+import { useForm } from "../../hooks/useForm";
 import { HeroCard } from "../components/HeroCard";
+import { getHeroByName } from "../helpers/getHeroByName";
 
 export const SearchPage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q = "" } = queryString.parse(location.search);
+  const heroes = getHeroByName(q);
+
+  const { searchText, onInputChange } = useForm({
+    searchText: q,
+  });
+
+  const onSearchSubmit = (e) => {
+    e.preventDefault();
+    //if (searchText.trim().length <= 1) return
+    navigate(`?q=${searchText}`);
+  };
+
   return (
     <>
       <h1>SearchPage</h1>
@@ -8,25 +28,36 @@ export const SearchPage = () => {
       <div className="row">
         <div className="col-5">
           <hr />
-          <form>
+          <form onSubmit={onSearchSubmit}>
             <input
               type="text"
               className="form-control"
               name="searchText"
               autoComplete="off"
               placeholder="search heroe"
+              value={searchText}
+              onChange={onInputChange}
             />
-            <button className="btn btn-outline-primary mt-1">search</button>
+            <button className="btn btn-outline-primary mt-2">search</button>
           </form>
         </div>
         <div className="col-7">
           <h4>Results:</h4>
+          {/*{
+            (q == '')
+              ? <div className="alert alert-primary">Search a heroe</div>
+              : ( heroes.length === 0 ) && <div className="alert alert-danger"> No se encuentra <b>{ q }</b> en la busqueda ... </div>
+          }*/}
+
+
+
           <hr />
-          <div className="alert alert-primary">Search a heroe</div>
-          <div className="alert alert-danger">
-            There´s not <b>hero</b> name ...
-          </div>
-          {/*<HeroCard />*/}
+
+          <div className="alert alert-primary" style={{opacity:0}}>Search a heroe</div>
+
+          {heroes.map((hero) => (
+            <HeroCard key={hero.id} {...hero} />
+          ))}
         </div>
       </div>
     </>
